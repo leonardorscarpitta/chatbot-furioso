dd607727b21fa4dfd38bcad765b5dd07d5a44d06
Leonardo <leonardorscarpitta@gmail.com>
2025-05-03 14:25:44 -0300

    style: change line up card size & update player info

--- front-end/src/components/main/lineup/CardLineUp.jsx
+++ front-end/src/components/main/lineup/CardLineUp.jsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function CardLineUp( props ) {
   return (
-    <div className="h-130 shadow-xl/20 text-white bg-gradient-to-b from-black to-gray-900 flex w-3xs flex-col gap-y-4 rounded-lg p-4">
+    <div className="h-135 shadow-xl/20 text-white bg-gradient-to-b from-black to-gray-900 flex w-3xs flex-col gap-y-4 rounded-lg p-4">
       <h4 className="flex gap-x-1 items-center">
           <Image src={props.nacionality} alt={`imagem da bandeira em que o jogador ${props.playerName} nasceu`} width={20} height={20} />
           {props.playerName} 
@@ -11,7 +11,7 @@ export default function CardLineUp( props ) {
         </small></h4>
       <Image width={400} height={417} src={props.playerImg} alt={props.altText} />
       <p className="text-base font-bold">{props.birthDate}</p>
-      <p className="text-justify">{props.description}</p>
+      <p>{props.description}</p>
     </div>
   )
 }
--- front-end/src/components/main/lineup/LineUp.jsx
+++ front-end/src/components/main/lineup/LineUp.jsx
@@ -6,7 +6,7 @@ export default function LineUp() {
       <h3 className="text-white text-2xl uppercase font-bold">Line UP dos Furiosos</h3>
       <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 flex-col gap-4 items-center justify-center">
 
-        <CardLineUp nacionality="https://img.icons8.com/color/48/brazil.png" playerName="FalleN" playerFunction="igl" playerImg="https://img-cdn.hltv.org/playerbodyshot/Wf26SO_o8nvnsLh0AqZXc5.png?ixlib=java-2.1.0&w=400&s=36b7189a4ae7b020d0acb087fd44777a" altText="imagem do jogador fallen da furia" description={`Após a saída de Andrei "arT" Piovezan, FalleN reassumiu a função de capitão da equipe, liderando as estratégias e desempenhando o papel de AWPER.`} birthDate="30/05/1991"/>
+        <CardLineUp nacionality="https://img.icons8.com/color/48/brazil.png" playerName="FalleN" playerFunction="igl | awper" playerImg="https://img-cdn.hltv.org/playerbodyshot/Wf26SO_o8nvnsLh0AqZXc5.png?ixlib=java-2.1.0&w=400&s=36b7189a4ae7b020d0acb087fd44777a" altText="imagem do jogador fallen da furia" description={`Após a saída de Andrei "arT" Piovezan, FalleN reassumiu a função de capitão da equipe, liderando as estratégias e desempenhando o papel de AWPER.`} birthDate="30/05/1991"/>
 
         <CardLineUp nacionality="https://img.icons8.com/color/48/brazil.png" playerName="yuurih" playerFunction="rifler" playerImg="https://img-cdn.hltv.org/playerbodyshot/i6UGhkYxrhutAOmWZT0-8O.png?ixlib=java-2.1.0&w=400&s=2cd696f6ff4baf5680a43d537214b6eb" altText="imagem do jogador yuurih da furia" description={`Com uma longa trajetória na FURIA, yuurih mantém seu papel de rifler, contribuindo significativamente para o desempenho da equipe.`} birthDate="22/12/1999"/>
 
@@ -14,7 +14,7 @@ export default function LineUp() {
 
         <CardLineUp nacionality="https://img.icons8.com/color/48/latvia.png" playerName="YEKINDAR" playerFunction="entry fragger" playerImg="https://img-cdn.hltv.org/playerbodyshot/rRclDPBXIMxFv2fv0dV0J0.png?ixlib=java-2.1.0&w=400&s=2b0f6209ca40efa081852b9d1d8e01c1" altText="imagem do jogador yekindar da furia" description={`YEKINDAR é um rifler letão agressivo, ex-Team Liquid e Virtus.pro, conhecido por abrir espaços e jogar como entry fragger com alta precisão e leitura de jogo.`} birthDate="04/12/1999"/>
 
-        <CardLineUp nacionality="https://img.icons8.com/color/48/kazakhstan.png" playerName="molodoy" playerFunction="awper" playerImg="https://img-cdn.hltv.org/playerbodyshot/qNyAd_xVHTTmbCAKPx-jPk.png?ixlib=java-2.1.0&w=400&s=b128ede878e462107c70590202b14139" altText="imagem do jogador fallen da furia" description="Jovem talento russo, molodoy assumiu a função de AWPer na equipe, trazendo uma nova dinâmica ao time." birthDate="10/01/2005"/>
+        <CardLineUp nacionality="https://img.icons8.com/color/48/kazakhstan.png" playerName="molodoy" playerFunction="rifler" playerImg="https://img-cdn.hltv.org/playerbodyshot/qNyAd_xVHTTmbCAKPx-jPk.png?ixlib=java-2.1.0&w=400&s=b128ede878e462107c70590202b14139" altText="imagem do jogador fallen da furia" description={`molodoy é oriundo do Cazaquistão, país natal de campeões mundiais como Abay HObbit e Qikert . Os primeiros passos no competitivo foram dados quando tinha apenas 15 anos.`} birthDate="10/01/2005"/>
 
       </div>
     </section>
